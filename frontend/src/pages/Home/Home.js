6bfc37122dd93717a5fe4f2ed3988d515ba5319c
Daniela Liang <artaffu@gmail.com>
2024-04-15 20:31:23 -0400

    some style

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,12 +1,24 @@
-import "./Home.css"
-
+import "./Home.css";
 
 function Home() {
-    return (
-        <p>
-            What is in your pantry?
-        </p>
-    );
+  return (
+    <div className="home">
+      <p>What is in your pantry?</p>
+      <div class="selectIngredients">
+        <label for="ingredients">Select Ingredients:</label>
+        <select id="ingredients" multiple>
+          <option value="Broccoli">Broccoli</option>
+          <option value="Corn">Corn</option>
+          <option value="Cucumber">Cucumber</option>
+          <option value="Tomato">Tomato</option>
+          <option value="Mushroom">Mushroom</option>
+          <option value="Garlic">Garlic</option>
+          <option value="Onion">Onion</option>
+          <option value="Brinjal">Brinjal</option>
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
